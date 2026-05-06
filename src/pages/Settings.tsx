07ae8616a9263c50bb10d58015edaf5c1726b8d6
Rayanne <rayanne.ernandez@globaltera.com.br>
2026-05-06 15:29:06 -0300

    resete

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -205,17 +205,9 @@ export function Settings() {
       }
 
       const defaultIds = ['flow_trend', 'hourly_flow', 'age_pyramid', 'gender_dist', 'attributes', 'journey', 'campaigns'];
-      const baseIds = resolved.ids && resolved.ids.length
+      const finalIds = resolved.ids && resolved.ids.length
         ? resolved.ids
         : defaultIds;
-      const configuredIds = new Set(resolved.ids ?? []);
-      const newWidgetIds = AVAILABLE_WIDGETS
-        .map((widget) => widget.id)
-        .filter((widgetId) => !configuredIds.has(widgetId));
-      const finalIds = [...baseIds];
-      for (const widgetId of newWidgetIds) {
-        if (!finalIds.includes(widgetId)) finalIds.push(widgetId);
-      }
 
       const active = finalIds
         .map((wid) => AVAILABLE_WIDGETS.find((w) => w.id === wid))
